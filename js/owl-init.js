5465a0fb8fc5007d8b74de15ec482b1fb1fd91a3
Michał Kupiecki <michal481216@gmail.com>
2019-06-26 12:32:13 +0200

    add files

---
+++ js/owl-init.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    //Init the carousel
+    initSlider();
+
+    function initSlider() {
+        $(".owl-carousel").owlCarousel({
+            items: 1,
+            loop: true,
+            autoplay: true,
+            onInitialized: startProgressBar,
+            onTranslate: resetProgressBar,
+            onTranslated: startProgressBar,
+            nav: true
+        });
+    }
+
+    function startProgressBar() {
+        // apply keyframe animation
+        $(".slide-progress").css({
+            width: "100%",
+            transition: "width 5000ms"
+        });
+    }
+
+    function resetProgressBar() {
+        $(".slide-progress").css({
+            width: 0,
+            transition: "width 0s"
+        });
+    }
+});
